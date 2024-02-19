73e74ffcf95adc9ce7fe1e5377d863bd2da1a42b
MuthuPillai <muthupillaimuthu123@gmail.com>
2024-02-19 16:13:31 +0530

    Summa dha potan

---
+++ Untitled-1.py
@@ -0,0 +1,36 @@
+#print("   /!")
+#print("  / !")
+#print(" /  !")
+#print("/___!")
+
+#Character_Name ="Muthu"
+#Character_Age = "27"
+#print("There once was a man named " + Character_Name + ",")
+#print("He was " + Character_Age + " years old. ")
+#print("He really liked the name " + Character_Name + ",")
+#print("But didn't like being " + Character_Age + ".")
+
+#print("Hello World!\nHello World!\nHello World!\nHello World!")
+#print("Hello" +"  "+"Muthu")
+#print("Hello "+ input("What is ur name?"))
+#python variables
+#name = input("What is ur name?")
+#print(name)
+#print(len ()))
+#name = input("What is ur name?")
+#length = length(name)
+#print(length)
+
+#print("Welcome to the Band Name Generator.")
+#city = input("which city u grew up in?")
+#print(city)
+#pet = input("what is name of ur pet")
+#print("your band name could be" + city + " " + pet)
+
+len("muthu saravanan")
+print(len)
+
+
+
+
+
